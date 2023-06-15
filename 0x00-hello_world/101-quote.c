f16a1320503183d0a172165c0221f2a66851e5ed
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-15 03:18:33 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, sizeof(message) - 1);
+write(STDERR_FILENO, message, 53);
 return (1);
 }
