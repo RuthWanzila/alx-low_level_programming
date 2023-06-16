98ee1e5e423c3fa7212c21893e70378315d75915
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-16 04:30:16 -0700

    lower then upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,8 +9,12 @@
 int main(void)
 {
 char c;
+/*print lowercase*/
 for (c = 'a'; c <= 'z'; ++c)
 putchar(c);
+/*print uppercase*/
+for (c = 'A'; c <= 'Z'; ++c)
+putchar (c);
 putchar('\n');
 return (0);
 }
