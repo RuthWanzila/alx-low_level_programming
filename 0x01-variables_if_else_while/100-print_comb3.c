817fa170b93cad268c4da20cf77804a1c31b00a6
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-16 05:22:50 -0700

    combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,20 @@
  */
 int main(void)
 {
-int i;
+int i, j;
 
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 8; ++i)
 {
-putchar(i + '0');
-if (i != 9)
+for (j = i+1; j <= 9; ++j)
+{
+int num = (i * 10) + j;
+putchar((num / 10) + '0');
+putchar((num % 10) + '0');
+if (num < 89)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
