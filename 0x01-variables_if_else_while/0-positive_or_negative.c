a8a34cc8beb96b3a00d967f908ecae9aeddb8524
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-16 04:04:42 -0700

    +ve or _ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,10 @@
  * Return: Always 0 (Success)
  *
  *more headers goes there
- *betty style doc for function main goes there */
+ *
+ *betty style doc for function main goes there
+ *
+ */
 int main(void)
 {
 int n;
