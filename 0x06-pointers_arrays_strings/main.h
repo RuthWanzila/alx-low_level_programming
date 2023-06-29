6c9e70cfc7d57d6f83f0802408f46dc7306c3526
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-29 04:53:01 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,8 +2,12 @@
 #define MAIN_H
 
 char *_strcat(char *dest, char *src);
-int _putchar(char c);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
+char *cap_string(char *s);
+char *leet(char *s);
+char *rot13(char *s);
 
 #endif /* MAIN_H */
