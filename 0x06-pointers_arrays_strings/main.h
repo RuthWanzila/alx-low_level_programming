a28b086ad5318bd9653c74e23559f8ec2bd724f5
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-29 08:05:29 -0700

    wuueh

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,8 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *s);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
 
 #endif /* MAIN_H */
