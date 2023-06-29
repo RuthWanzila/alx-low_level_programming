30391a34b822b3f0968ad9ef401e22e358fb9b2b
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-29 05:01:51 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int a, b;
+
 for (a = 0; dest[a] != '\0'; a++)
 ;
-for (b = 0; src[b] != '\0' && n > 0; b++, n--, i++)
+for (b = 0; b < n && src[b] != '\0'; b++)
 {
-dest[a] = src[b];
+dest[a + b] = src[b];
 }
 return (dest);
 }
