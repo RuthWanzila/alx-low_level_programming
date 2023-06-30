9c674d4ca1afedf9b2e1c2032cb1997ee24cb785
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-30 04:45:46 -0700

    _putchar

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,5 @@ char *rot13(char *s);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
-
+int _putchar(char c);
 #endif /* MAIN_H */
