70a5f1425b2d66d059545e3ab9a035eb9ffe7fe4
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-30 04:55:37 -0700

    printf

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,5 @@ void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 int _putchar(char c);
+int printf(const char *format, ...);
 #endif /* MAIN_H */
