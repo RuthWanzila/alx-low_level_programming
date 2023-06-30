4b4549b7c0c6d0af24f1ed9d72648be7177ce0b6
RuthWanzila <wanzilaruth533@gmail.com>
2023-06-30 04:41:17 -0700

    how it feels

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,18 +2,18 @@
 /**
 * print_number - prints an integer
 *@n:integer to be printed
-*
+*Return: 0
 */
 void print_number(int n)
 {
-unsigned int num = n;
 if (n < 0)
 {
 _putchar('-');
-num = -num;
+n = -n;
 }
-
-if ((num / 10) > 0)
-print_number(num / 10);
-_putchar((num % 10) + '0');
+if (n / 10 != 0)
+{
+print_number(n / 10);
+}
+_putchar(n % 10 + '0');
 }
