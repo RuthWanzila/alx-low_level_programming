bc3625a35b6f85b4242cc235ce65d8d1848af0cb
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-03 05:35:05 -0700

    change

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,17 +9,16 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int a, b;
-while (b = 0; needle[b] != '\0')
+int a;
+b = 0;
+while (needle[b] != '\0')
 b++;
 while (*haystack)
 {
 for (a = 0; needle[a]; a++)
 {
 if (haystack[b] != needle[b])
-{
 break;
-}
 }
 if (a == b)
 {
