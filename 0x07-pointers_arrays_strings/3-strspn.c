cff2ab400026290330ac860a288a21b3ce2be964
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-03 04:07:45 -0700

    commit changes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,4 +9,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+unsigned int k;
+int j;
+
+while (*s)
+{
+for (j = 0; accept[j]; j++)
+{
+if (accept[j] == *s)
+{
+k++
+break;
+}
+else if ((accept[j + 1]) == '\0')
+{
+return (k);
+}
+}
+s++;
+}
+return (k);
+
 }
