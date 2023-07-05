2c4703f78007d00547f91bf96684959929d82355
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-05 08:39:25 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -28,21 +28,21 @@ return (is_prime_assist(n, 5));
 /**
  * is_prime_assist - helper function
  * @n: no.
- * @factor: divisor
+ * @i: divisor
  * Return: 1 if prime, 0 otherwise
  */
-int is_prime_assist(int n, int factor)
+int is_prime_assist(int n, int i)
 {
-if (factor * factor > n)
+if (i * i > n)
 {
 return (1);
 }
-else if (n % factor == 0 || n % (factor) == 0)
+else if (n % i == 0 || n % (i + 2) == 0)
 {
 return (0);
 }
 else
 {
-return (is_prime_assist(n, factor + 6));
+return (is_prime_assist(n, i + 6));
 }
 }
