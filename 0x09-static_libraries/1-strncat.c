bf80cef63061557c525d58fbf713b852cc6577bc
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-07 00:08:48 -0700

    libmy.a

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strncat - concatenates two strings pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *@n: number of elements to concatenate in
+ * Return: returns pointer to @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int a, b;
+
+for (a = 0; dest[a] != '\0'; a++)
+;
+for (b = 0; b < n && src[b] != '\0'; b++)
+{
+dest[a + b] = src[b];
+}
+return (dest);
+}
