e073e43343ae2de91dc087ea7e679d336fcc0e9f
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-07 05:14:29 -0700

    hey

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -6,10 +6,15 @@
 */
 void _puts(char *str)
 {
-while (*str != '\0')
+int count = 0;
+while (count >= 0)
 {
-_putchar(*str);
-str++;
-}
+if (str[count] == '\0')
+{
 _putchar('\n');
+break;
+}
+_putchar(str[count]);
+count++;
+}
 }
