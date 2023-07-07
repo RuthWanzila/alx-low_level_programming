0e015c4e644f9c92a0e4b6fb2cb815dd81dcf11c
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-07 02:00:07 -0700

    tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints its name
+ * @argc: count
+ * argv: vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
