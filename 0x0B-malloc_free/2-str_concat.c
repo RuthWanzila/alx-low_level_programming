a89af749f5281287fcd08ebfc6b7729a2960872c
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-10 04:16:19 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,12 +8,21 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int a, b;
-for (a = 0; s1[a] != '\0'; a++)
-;
-for (b = 0; s2[b] != '\0'; b++)
-{
-s1[a+b] =s2[b];
-}
-return (s1);
+if (s1 == '\0')
+s1 = "";
+if (s2 == '\0')
+s2 = "";
+
+size_t len1 = strlen(s1);
+size_t len2 = strlen(s2);
+
+char *res = malloc(len1 + len2 + 1);
+if (res == '\0')
+return ('\0');
+
+memcpy(res, s1, len1);
+memcpy(res + len1, s2, len2);
+res[len1 + len2] = '\0';
+
+return (res);
 }
