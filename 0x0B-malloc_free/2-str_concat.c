1c0e599dfc120296e6fb8aba442ddebc25301c1b
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-10 04:27:30 -0700

    correct

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,15 +9,17 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+size_t len1, len2;
+char *res;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-size_t len1 = strlen(s1);
-size_t len2 = strlen(s2);
+len1 = strlen(s1);
+len2 = strlen(s2);
 
-char *res = malloc(len1 + len2 + 1);
+res = malloc(len1 + len2 + 1);
 if (res == NULL)
 return (NULL);
 
