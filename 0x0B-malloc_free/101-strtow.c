0b4d3c4ec6dda30cd8b40a58f8a6a6b8a2766c49
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-11 01:06:05 -0700

    >40

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,53 +6,54 @@
  */
 char **strtow(char *str)
 {
-char **words_array;
-int j, start_pos, end_pos, ptr_index; 
-int num_words, i;
-num_words = 0;
-start_pos = 0;
-end_pos;
-ptr_index = 0;
-if (str == NULL || str[0] == '\0')
+int a, c, b, k, count, m, word;
+char **p;
+char *x;
+c = 0;
+b = 0;
+a = 0;
+count = 0;
+if (*str == '\0' || str == NULL)
+return (NULL);
+for (a = 0; str[a] != '\0'; a++)
 {
+if (str[a] == ' ' && (str[a + 1] != ' ' || str[a + 1] == '\0'))
+c++;
+}
+p = (char **)malloc((c + 1) * sizeof(char *));
+if (p == NULL)
+return (NULL);
+for (word = 0; str[word] && b <= c; word++)
+{
+count = 0;
+if (str[word] != ' ')
+{
+for (a = word; str[a] != '\0'; a++)
+{
+if (str[a] == ' ')
+break;
+count++;
+}
+*(p + b) = (char *)malloc((count + 1) * sizeof(char));
+if (*(p + b) == NULL)
+{
+for (k = 0; k <= j; k++)
+{
+x = p[k];
+free(x);
+}
+free(p);
 return (NULL);
 }
-for (i = 0; str[i] != '\0'; i++)
+for (m = 0; word < a; word++)
 {
-if (((i > 0) && (str[i - 1] != ' ')) || (str[i] != ' '))
-{
-num_words++;
+p[b][m] = str[word];
+m++;
+}
+p[b][m] = '\0';
+b++;
 }
 }
-words_array = (char **)malloc((num_words + 1) * sizeof(char *));
-if (words_array == NULL)
-{
-return (NULL);
-}
-for (i = 0; str[i] != '\0'; i++)
-{
-if (((i > 0) && (str[i - 1] == ' ')) || (start_pos == 0))
-{
-start_pos = i;
-}
-else if ((str[i + 1] == '\0') || (str[i + 1] == ' '))
-{
-end_pos = i + 1;
-words_array[ptr_index] = (char *)malloc((end_pos - start_pos) + 1);
-if (words_array[ptr_index] == NULL)
-{
-for (j = 0; j < ptr_index; j++)
-{
-free(words_array[j]);
-}
-free(words_array);
-return (NULL);
-}
-memcpy(words_array[ptr_index], &(str[start_pos]), ((end_pos - start_pos)));
-words_array[ptr_index][(end_pos - start_pos)] = '\0';
-ptr_index++;
-}
-}
-words_array[num_words] = NULL;
-return (words_array);
+p[b] = NULL;
+return (p);
 }
