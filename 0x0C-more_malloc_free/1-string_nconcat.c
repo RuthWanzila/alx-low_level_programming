a0126ed7fc48e393e83057c768021b25faac882b
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-13 02:32:52 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,9 +8,8 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len1, len2, x, y;
-len1 = 0;
-len2 = 0;
+unsigned int len1, x;
+len1 = n;
 char *concatenated;
 if (s1 == NULL)
 {
@@ -20,25 +19,22 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-len1 = strlen(s1);
-len2 = strlen(s2);
-if (n >= len2)
+for (x = 0; s1[x]; x++)
 {
-n = len2;
+len1++;
 }
-concatenated = malloc(sizeof(char) * (len1 + n + 1));
+concatenated = malloc(sizeof(char) * (len1 + 1));
 if (concatenated == NULL)
 {
 return (NULL);
 }
-for (x = 0; x < len1; x++)
+for (x = 0; s1[x]; x++)
 {
-concatenated[i] = s1[i];
+concatenated[len1++] = s1[x];
 }
-for (y = 0; x < n; y++, x++)
-{
-concatenated[x] = s2[y];
-concatenated[x] = '\0';
-}
+for (x = 0; s2[x] && x < n; x++)
+concatenated[len1++] = s2[x];
+concatenated[len1] = '\0';
+
 return (concatenated);
 }
