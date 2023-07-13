fcfd31c6fcbe1cce5784c436831ccb6387051795
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-13 04:54:39 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,4 +7,20 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
 
+void *ptr = malloc(nmemb * size);
+
+if (ptr == NULL)
+{
+
+return (NULL);
+}
+
+memset(ptr, 0, nmemb * size);
+
+return (ptr);
+}
