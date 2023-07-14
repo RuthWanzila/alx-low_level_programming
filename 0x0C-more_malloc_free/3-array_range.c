b0f25e20d872933ba1d517aaca30e3490c778aaf
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-13 23:24:57 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,18 +7,19 @@
  */
 int *array_range(int min, int max)
 {
+int size, *arr, i;
 if (min > max)
 {
 return (NULL);
 }
-int size = max - min + 1;
-int *arr = malloc(size * sizeof(int));
+size = max - min + 1;
+arr = malloc(size * sizeof(int));
 
 if (arr == NULL)
 {
 return (NULL);
 }
-for (int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 arr[i] = min + i;
 }
