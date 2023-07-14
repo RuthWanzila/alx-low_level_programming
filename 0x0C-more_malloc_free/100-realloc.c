2eaa0fa82246f1230a501981749c353cd06809fd
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-13 23:31:40 -0700

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,6 +8,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+void *new_ptr;
+unsigned int min_size;
 if (ptr == NULL)
 {
 return (malloc(new_size));
@@ -24,14 +26,13 @@ if (new_size == old_size)
 return (ptr);
 }
 
-void *new_ptr = malloc(new_size);
+new_ptr = malloc(new_size);
 
 if (new_ptr == NULL)
 {
 return (NULL);
 }
-
-unsigned int min_size = (old_size < new_size) ? old_size : new_size;
+min_size = (old_size < new_size) ? old_size : new_size;
 memcpy(new_ptr, ptr, min_size);
 
 free(ptr);
