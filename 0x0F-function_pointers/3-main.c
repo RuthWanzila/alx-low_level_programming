ca7997a7ffb0e3cd6c11d56ef965cab1f59557d8
RuthWanzila <wanzilaruth533@gmail.com>
2023-07-19 23:55:36 -0700

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,26 +3,45 @@
 /**
  * main - Entry point. Performs simple operations.
  * @argc: The number of arguments passed.
-i * @argv: An array of pointers to the arguments.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-int num1;
-int num2;
-int result;
-int (*op_func)(int, int);
-
+int num1, num2, result;
+char *op;
 if (argc != 4)
 {
-printf("Error\n");
-exit(98);
+printf("Usage: %s <num1> <operator> <num2>\n", argv[0]);
+return (1);
 }
 num1 = atoi(argv[1]);
+op = argv[2];
 num2 = atoi(argv[3]);
-op_func = get_op_func(argv[2]);
-result = op_func(num1, num2);
+if (op[1] != '\0')
+{
+printf("Error: Operator must be a single character\n");
+return (1);
+}
+switch (*op)
+{
+case '+':
+result = op_add(num1, num2);
+break;
+case '-':
+result = op_sub(num1, num2);
+break;
+case '*':
+result = op_mul(num1, num2);
+break;
+case '/':
+result = op_div(num1, num2);
+break;
+default:
+printf("Error: Unknown operator `%c'\n", *op);
+return (1);
+}
 printf("%d\n", result);
 return (0);
 }
