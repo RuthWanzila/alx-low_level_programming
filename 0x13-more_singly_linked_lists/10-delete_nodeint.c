c4c6528275d431031463211f36fbcf2773c01bb4
RuthWanzila <wanzilaruth533@gmail.com>
2023-08-02 03:23:31 -0700

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,12 +8,13 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+unsigned int i;
 if (head == NULL || *head == NULL)
 {
 return (-1);
 }
 listint_t *current_node = *head, *previous_node = NULL;
-for (unsigned int i = 0; current_node != NULL && i < index; i++)
+for (i = 0; current_node != NULL && i < index; i++)
 {
 previous_node = current_node;
 current_node = current_node->next;
