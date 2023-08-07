660ecb5f62b5f7537f634f84fbdc5b2cd5366e93
RuthWanzila <wanzilaruth533@gmail.com>
2023-08-07 04:03:14 -0700

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,52 @@
 #include "main.h"
 
 /**
- * @read_textfile - reads a text file and
- * @prints it to the POSIX standard output.
- * @filename - string of characcters of files name.
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: string of characcters of files name.
  * @letters: number of letters it should read and print
  * Return:  actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+int x;
+ssize_t bytes_read, bytes_written;
+char *buff;
+
 if (filename == NULL)
 return (0);
 
-FILE *file = fopen(filename, "r");
-if (file == NULL)
+x = open(filename, O_RDONLY);
+if (x == -1)
 return (0);
 
-char buffer[letters + 1];
-ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
-if (bytesRead <= 0)
+buff = malloc(sizeof(char) * (letters + 1));
+if (buff == NULL)
 {
-fclose(file);
+close(x);
 return (0);
 }
-ssize_t bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
-if (bytesWritten != bytesRead)
+
+bytes_read = read(x, buff, letters);
+if (bytes_read == -1)
 {
-fclose(file);
+free(buff);
+close(x);
 return (0);
 }
-fclose(file);
-return (bytesRead);
+
+buff[bytes_read] = '\0';
+
+bytes_written = write(STDOUT_FILENO, buff, bytes_read);
+if (bytes_written == -1 || (size_t)bytes_written != bytes_read)
+{
+free(buff);
+close(x);
+return (0);
+}
+
+free(buff);
+close(x);
+
+return (bytes_written);
 }
