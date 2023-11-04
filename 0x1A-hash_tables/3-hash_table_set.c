4091638098aa5ee0ee59854d767878863afa71c6
RuthWanzila <wanzilaruth533@gmail.com>
2023-11-04 03:52:20 -0700

    get

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,41 +12,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long int index;
 hash_node_t *node; /* head */
 hash_node_t *walk;
-
-if( !key || !value || !ht || !ht->array)
+if (!key || !value || !ht || !ht->array)
 return (0);
-
-
 /* size structure */
 index = key_index((unsigned char *)key, ht->size);
 if (ht->array[index])
 {
 /* walking in the linked list */
 walk = ht->array[index];
-/* int strcmp(const char *, const char *) */
 /* if the node is occupy, walk to next */
-while (walk && strcmp(walk->key, key) != 0 )
+while (walk && strcmp(walk->key, key) != 0)
 walk = walk->next;
-
-if (walk && strcmp(walk->key, key) == 0 )
-{	/* value must be duplicated */
+if (walk && strcmp(walk->key, key) == 0)
+/* value must be duplicated */
 free(walk->value);
-walk->value =strdup(value);
+walk->value = strdup(value);
 /* element added */
 return (1);
-}
 }
 /* element not added */
 node = malloc(sizeof(hash_node_t));
 if (node == NULL)
 return (0);
-
 node->key = strdup(key);
 if (node->key == NULL)
-{
 free(node);
 return (0);
-}
 node->value = strdup(value);
 if (node->value == NULL)
 {
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,32 +3,34 @@
 * hash_table_get - Retrieves the value associated with a key
 * @ht: The hash table to look into
 * @key: The key to search for
-* Return: The value associated with the key, or NULL if the key couldn't be found
+* Return:value associated with the key, or NULL if the key couldn't be found
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-unsigned int idx;
-hash_node_t *curr_node;
+unsigned int index;
+hash_node_t *node;
 
 if (!key || !ht || !ht->array)
 return (NULL);
 
-/* Compute the idx of the key */
-idx = key_idx((unsigned char *)key, ht->size);
+/* Compute the index of the key */
+index = key_index((unsigned char *)key, ht->size);
 
-/* Traverse the linked list at the idx position */
-curr_node = ht->array[idx];
-while (curr_node != NULL)
+/* Set 'node' to point to the linked list at the computed index */
+node = ht->array[index];
+
+/* Traverse the linked list */
+while (node != NULL)
 {
 /* Compare the key with the current node's key */
-if (strcmp(curr_node->key, key) == 0)
+if (strcmp(node->key, key) == 0)
 {
 /* Return the value associated with the key */
-return (curr_node->value);
+return (node->value);
 }
 
 /* Move to the next node in the linked list */
-curr_node = curr_node->next;
+node = node->next;
 }
 
 /* The key couldn't be found */
